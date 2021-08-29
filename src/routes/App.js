11678521e2f59cf23e814cbcfd20b40bb71478cd
seudosan <marco2015.sandoval@gmail.com>
2021-08-29 19:55:35 -0300

    :building_construction: Added layout
    A main layout has been added

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from '../components/Layout';
 import Home from '../containers/Home';
 import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
